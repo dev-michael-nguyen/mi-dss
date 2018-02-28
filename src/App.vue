fb433a0955cf7fa5c477a960df59d83dcfb28c1c
Michael Nguyen <Michael.Nguyen@feisystems.com>
2018-02-28 15:13:57 -0500

    clean up App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,18 @@
 <template>
   <v-app>
     <v-navigation-drawer
+      app
+      v-model="showDrawer"
       persistent
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      v-model="drawer"
       enable-resize-watcher
       fixed
-      app
-    >
+      :clipped="clipped"
+      :mini-variant="miniVariant">
       <v-list>
         <v-list-tile
           value="true"
           v-for="(item, i) in items"
-          :key="i"
-        >
+          :key="i">
           <v-list-tile-action>
             <v-icon v-html="item.icon"></v-icon>
           </v-list-tile-action>
@@ -26,34 +24,29 @@
     </v-navigation-drawer>
     <v-toolbar
       app
-      :clipped-left="clipped"
-    >
-      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      :clipped-left="clipped">
+      <v-toolbar-side-icon @click.stop="showDrawer = !showDrawer"></v-toolbar-side-icon>
       <v-btn icon @click.stop="miniVariant = !miniVariant">
         <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
       </v-btn>
-      <v-btn icon @click.stop="clipped = !clipped">
+      <!-- <v-btn icon @click.stop="clipped = !clipped">
         <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>remove</v-icon>
-      </v-btn>
+      </v-btn> -->
       <v-toolbar-title v-text="title"></v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
+      <!-- <v-btn icon @click.stop="showRightDrawer = !showRightDrawer">
         <v-icon>menu</v-icon>
-      </v-btn>
+      </v-btn> -->
     </v-toolbar>
     <v-content>
       <router-view/>
     </v-content>
-    <v-navigation-drawer
-      temporary
-      :right="right"
-      v-model="rightDrawer"
-      fixed
+    <!-- <v-navigation-drawer
       app
-    >
+      v-model="showRightDrawer"
+      temporary
+      fixed
+      :right="right">
       <v-list>
         <v-list-tile @click="right = !right">
           <v-list-tile-action>
@@ -62,30 +55,30 @@
           <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
         </v-list-tile>
       </v-list>
-    </v-navigation-drawer>
-    <v-footer :fixed="fixed" app>
+    </v-navigation-drawer> -->
+    <!-- <v-footer
+      app
+      fixed>
       <span>&copy; 2017</span>
-    </v-footer>
+    </v-footer> -->
   </v-app>
 </template>
 
 <script>
 export default {
+  name: 'App',
   data () {
     return {
-      clipped: false,
-      drawer: true,
-      fixed: false,
-      items: [{
-        icon: 'bubble_chart',
-        title: 'Inspire'
-      }],
+      title: 'DSS',
+      showDrawer: true,
+      clipped: true,
       miniVariant: false,
+      showRightDrawer: false,
       right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
+      items: [
+        { icon: 'bubble_chart', title: 'Inspire' }
+      ]
     }
-  },
-  name: 'App'
+  }
 }
 </script>
